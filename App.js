fe0e155f95d94aed0b3f00a3858547f4b0986120
Jean-jacques AKAKPO <akakpo.jeanjacques@gmail.com>
2019-05-30 14:03:22 +0200

    Bug d'affichage résolu

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
-import AppNavigator from './navigation/AppNavigator'
+import { View, Text, StyleSheet } from "react-native";
+import AppNavigator from "./navigation/AppNavigator";
 
 export default class App extends React.Component {
   state = {};
 
   render () {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <AppNavigator/>
+      <View style={styles.container}>
+        <AppNavigator />
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
